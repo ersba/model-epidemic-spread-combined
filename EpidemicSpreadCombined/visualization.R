2244b86ac6b21b0ce42908e93f6493ac8fdfc4b3
ersba <ersan.bara@gmail.com>
2024-04-08 12:46:28 +0200

    added rstudio visualization

---
+++ EpidemicSpreadCombined/visualization.R
@@ -0,0 +1,21 @@
+library(ggplot2)
+library(dplyr)
+library(rstudioapi)
+
+current_path <- dirname(rstudioapi::getSourceEditorContext()$path)
+
+file_path <- file.path(current_path, "bin", "Debug", "net6.0", "Host.csv")
+
+data <- read.csv(file_path)
+
+data <- data %>% filter(Tick >= 1)
+
+stages <- c("Anfällige", "Exponierte", "Infizierte", "Genesene", "Verstorbene")
+
+ggplot(data, aes(x = Tick, group = MyStage, colour = as.factor(MyStage))) +
+  geom_line(stat = "count", size = 1.1) +  # Erhöht die Linienstärke
+  scale_colour_manual(values = c("blue", "orange", "red", "green", "black"),
+                      labels = stages,
+                      name = "Stadium") +
+  labs(x = "Zeit (Ticks)", y = "Anzahl der Agenten", colour = "dark") +
+  theme_minimal()
